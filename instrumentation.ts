56aeb770939831b66cd421118dacc240c7d49de9
Dmitrii <dmitriysemenov1994th@gmail.com>
2026-06-03 13:35:28 +0300

    rm instrumentation

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-// export async function register() {
-//   if (process.env.NEXT_RUNTIME === "nodejs") {
-//     const { server } = await import("./src/lib/tests/server");
-//     server.listen();
-//   }
-// }
